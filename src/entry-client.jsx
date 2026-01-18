f9b0d9a38917d4334e25a347b94858840f1ecd30
geodem <geodem.dev@gmail.com>
2026-01-19 01:35:23 +0800

    dddd

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 hydrateRoot(
   document.getElementById("root"),
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
